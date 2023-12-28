5909338d66e9b8a693fa4d4e4f8f2e318e629b09
pedrool00 <pol.2000.po@gmail.com>
2023-12-28 18:53:30 +0100

    Little Fix

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -3,26 +3,30 @@ import FullScreenSection from "./FullScreenSection";
 import { Box, Heading, useMediaQuery } from "@chakra-ui/react";
 import Card from "./Card";
 
-// Array of skill objects
+// Array of project objects
 const skills = [
   {
     title: "Cybersecurity",
-    description: "Knowledgeable about cybersecurity frameworks...",
+    description:
+      "Knowledgeable about cybersecurity frameworks, and able to implement security controls to ensure compliance with international regulations such as GDPR. Experienced identifying threats, risks, vulnerabilities, security hardening, threat modeling, and incident escalation and response. Proficient in the use of the Linux command line, SQL, Python, Sniffers, SIEM tools, and many more.",
     getImageSrc: () => require("../images/photo1.jpg"),
   },
   {
     title: "Full Stack",
-    description: "Experienced in building robust and scalable web applications...",
+    description:
+      "Experienced in building robust and scalable web applications. Proficient in developing and integrating user-friendly interfaces, designing efficient databases, and implementing server-side logic. On the front-end, I excel in HTML, CSS, and JavaScript frameworks such as React. As for the back-end, I am experienced using Node.js and Python, along with frameworks such as Django.",
     getImageSrc: () => require("../images/photo2.jpg"),
   },
   {
     title: "Data Science and AI",
-    description: "Skilled as a Data Scientist, with expertise in utilizing Excel...",
+    description:
+      "Skilled as a Data Scientist, with expertise in utilizing Excel, SQL, Python, and AI to unlock the value hidden in complex datasets. With a strong background in data analysis, statistical modeling, machine learning, and cloud platforms, I specialize in solving business problems and driving data-informed decision-making at scale. I am an effective communicator, capable of translating technical findings into meaningful insights.",
     getImageSrc: () => require("../images/photo3.jpg"),
   },
   {
     title: "Elite Athlete",
-    description: "I have been the Spanish national champion in the 800m...",
+    description:
+      "I have been the Spanish national champion in the 800m, representing my country at the highest level. I am honored to have been sponsored by New Balance, a leading global brand. Additionally, in my collegiate career, I studied in the US at TXST University, where I was recognized as the Student-Athlete of the Year due to my athletic achievements. As the team captain, I did my best inspiring my teammates to excel both on and off the track.",
     getImageSrc: () => require("../images/photo4.jpg"),
   },
 ];
